ef0ee134d579be21d8bd9adf31061cc37a6c751f
Cimp1 <105791417+Cimp1@users.noreply.github.com>
2022-05-22 22:01:47 +0300

    added Citirea

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Atestat.NET.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -146,6 +146,16 @@ namespace Atestat.NET.Properties {
         internal static System.Drawing.Bitmap Buton_Teorie {
             get {
                 object obj = ResourceManager.GetObject("Buton_Teorie", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Citire {
+            get {
+                object obj = ResourceManager.GetObject("Citire", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
